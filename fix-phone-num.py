bb51309aa6d30349d5a83f55d37a8de0cfd16dcc
Advik Katiyar <ak54932@my.utexas.edu>
2026-04-07 00:54:35 -0500

    Fix phone number formatting bug

--- fix-phone-num.py
+++ fix-phone-num.py
@@ -30,6 +30,34 @@ def test_fix_phone_num():
 def test_fix_phone_num2():
     assert fix_phone_num("5554429876") == '(555) 442 9876'
 
+import pytest
+
+def fix_phone_num(phone_num_to_fix):
+    if not phone_num_to_fix.isdigit():
+        raise ValueError(f"phone number must only contain digits; got \"{phone_num_to_fix}\"")
+
+    if len(phone_num_to_fix) == 11:
+        if phone_num_to_fix[0] != "1":
+            raise ValueError(f"phone numbers of length 11 must begin with a 1, got \"{phone_num_to_fix}\"")
+        phone_num_to_fix = phone_num_to_fix[1:]
+    elif len(phone_num_to_fix) != 10:
+        raise ValueError(f"phone number must be length 10; got \"{phone_num_to_fix}\" which is of length {len(phone_num_to_fix)}")
+
+    area_code = phone_num_to_fix[0:3]
+    three_part = phone_num_to_fix[3:6]
+    four_part = phone_num_to_fix[6:]
+
+    fixed_num = "(" + area_code + ")" + " " + three_part + " " + four_part
+
+    return fixed_num
+
+
+def test_fix_phone_num():
+    assert fix_phone_num("5125558823") == '(512) 555 8823'
+
+def test_fix_phone_num2():
+    assert fix_phone_num("5554429876") == '(555) 442 9876'
+
 def test_fix_phone_num3():
     assert fix_phone_num("3216543333") == '(321) 654 3333'
 
